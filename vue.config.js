e043f411ccb71e0ddfbcd603cf06d99498ce1505
Ruslan Melnyk <ruslan_melnyk@ukr.net>
2021-03-31 18:47:26 +0300

    add deploy file

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/brighta/'
+    : '/',
   configureWebpack: {
     resolve: {
       extensions: ['.js', '.vue', '.json'],
